4318f40bace65e2a3094578951286bd4e4cb65d5
Arzoter <arthur.cloux@gmail.com>
2024-03-12 18:35:07 +0100

    feat: initialized dockerized node express & added json response system

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Endpoint pour récupérer les informations de score de match
+app.get('/matches', (req, res) => {
+    // Construire le chemin complet vers le fichier JSON
+    const filePath = path.join(__dirname, 'jsons', 'matches.json');
+
+    // Lire le fichier JSON contenant les informations de score de match
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading matches.json:', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        // Convertir les données JSON en objet JavaScript
+        const matches = JSON.parse(data);
+
+        // Envoyer les informations de score de match en réponse à la requête
+        res.json(matches);
+    });
+});
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ dockerfiles/node.Dockerfile
@@ -0,0 +1,24 @@
+# Utiliser une image Node.js comme base
+FROM node:lts-bookworm-slim
+
+# Créer le répertoire de travail
+WORKDIR /usr/src/app
+
+
+
+# Copier les dépendances du package.json et du package-lock.json
+COPY ../backend/package*.json ./
+COPY ../backend/server.js ./
+COPY ../backend/jsons/*.json ./jsons/
+
+# Installer les dépendances
+RUN npm install
+
+# Copier le reste du code source de l'application
+# COPY . .
+
+# Exposer le port 3000
+EXPOSE 3000
+
+# Commande pour démarrer l'application
+CMD [ "npm", "start" ]
